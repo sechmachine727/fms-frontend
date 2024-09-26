42f26d70c7a2024b7e4f8dac496006149b3a50e3
khsnh <ngx.k2210001@gmail.com>
2024-09-26 20:02:18 +0700

    Training Program List UX/UI

--- src/stores/topicStore.js
+++ src/stores/topicStore.js
@@ -52,3 +52,149 @@ export const assessmentScheme = [
     { assessmentName: 'Project', quantity: 1, weightedNumber: 30, note: 'Grading of the course project.' },
     { assessmentName: 'Participation', quantity: 1, weightedNumber: 10, note: 'Participation in class activities.' },
 ];
+
+export const TrainingProgramStore = {
+    getData() {
+        return[
+            {
+                id: 1,
+                CourseCode: 'FR_CP-KS',
+                TrainingName: 'Capstone Project FU KS',
+                Region:'FSA.HN',
+                TechnicalGroup:'Others',
+                Topic:'CPL_P-NET',
+                status: 'Active',
+                LastModifiedDate: '2015-09-13',
+                LastModifiedBy: 'KhanhNN44',
+            },
+            {
+                id: 2,
+                CourseCode: 'FR_CP-KS',
+                TrainingName: 'Capstone Project FU KS',
+                Region:'FSA.HN',
+                TechnicalGroup:'Others',
+                Topic:'CPL_P-NET',
+                status: 'Active',
+                LastModifiedDate: '2015-09-13',
+                LastModifiedBy: 'KhanhNN44',
+            },
+            {
+                id: 3,
+                CourseCode: 'FR_CP-KS',
+                TrainingName: 'Capstone Project FU KS',
+                Region:'FSA.HN',
+                TechnicalGroup:'Others',
+                Topic:'CPL_P-NET',
+                status: 'Active',
+                LastModifiedDate: '2015-09-13',
+                LastModifiedBy: 'KhanhNN44',
+            },
+            {
+                id: 4,
+                CourseCode: 'FR_CP-KS',
+                TrainingName: 'Capstone Project FU KS',
+                Region:'FSA.HN',
+                TechnicalGroup:'Others',
+                Topic:'CPL_P-NET',
+                status: 'Active',
+                LastModifiedDate: '2015-09-13',
+                LastModifiedBy: 'KhanhNN44',
+            },
+            {
+                id: 5,
+                CourseCode: 'FR_CP-KS',
+                TrainingName: 'Capstone Project FU KS',
+                Region:'FSA.HN',
+                TechnicalGroup:'Others',
+                Topic:'CPL_P-NET',
+                status: 'Active',
+                LastModifiedDate: '2015-09-13',
+                LastModifiedBy: 'KhanhNN44',
+            },
+            {
+                id: 6,
+                CourseCode: 'FR_CP-KS',
+                TrainingName: 'Capstone Project FU KS',
+                Region:'FSA.HN',
+                TechnicalGroup:'Others',
+                Topic:'CPL_P-NET',
+                status: 'Active',
+                LastModifiedDate: '2015-09-13',
+                LastModifiedBy: 'KhanhNN44',
+            },
+            {
+                id: 7,
+                CourseCode: 'FR_CP-KS',
+                TrainingName: 'Capstone Project FU KS',
+                Region:'FSA.HN',
+                TechnicalGroup:'Others',
+                Topic:'CPL_P-NET',
+                status: 'Active',
+                LastModifiedDate: '2015-09-13',
+                LastModifiedBy: 'KhanhNN44',
+            },
+            {
+                id: 8,
+                CourseCode: 'FR_CP-KS',
+                TrainingName: 'Capstone Project FU KS',
+                Region:'FSA.HN',
+                TechnicalGroup:'Others',
+                Topic:'CPL_P-NET',
+                status: 'Active',
+                LastModifiedDate: '2015-09-13',
+                LastModifiedBy: 'KhanhNN44',
+            },
+            {
+                id: 9,
+                CourseCode: 'FR_CP-KS',
+                TrainingName: 'Capstone Project FU KS',
+                Region:'FSA.HN',
+                TechnicalGroup:'Others',
+                Topic:'CPL_P-NET',
+                status: 'Active',
+                LastModifiedDate: '2015-09-13',
+                LastModifiedBy: 'KhanhNN44',
+            },
+            {
+                id: 10,
+                CourseCode: 'FR_CP-KS',
+                TrainingName: 'Capstone Project FU KS',
+                Region:'FSA.HN',
+                TechnicalGroup:'Others',
+                Topic:'CPL_P-NET',
+                status: 'Active',
+                LastModifiedDate: '2015-09-13',
+                LastModifiedBy: 'KhanhNN44',
+            },
+        ]
+    },
+
+    getTrainingProgramsSmall() {
+        return Promise.resolve(this.getData().slice(0, 10));
+    },
+
+    getTrainingProgramsMedium() {
+        return Promise.resolve(this.getData().slice(0, 50));
+    },
+
+    getTrainingProgramsLarge() {
+        return Promise.resolve(this.getData().slice(0, 200));
+    },
+
+    getTrainingProgramsXLarge() {
+        return Promise.resolve(this.getData());
+    },
+
+    getTrainingPrograms(params) {
+        const queryParams = params
+            ? Object.keys(params)
+                .map((k) => encodeURIComponent(k) + '=' + encodeURIComponent(params[k]))
+                .join('&')
+            : '';
+
+        return fetch('https://www.primefaces.org/data/customers?' + queryParams).then((res) => res.json());
+    },
+    getTrainingProgramsDetail(id) {
+        return Promise.resolve(this.getData().find((topic) => topic.id === id));
+    },
+};
