fa12acd3d83b29cded2710de61ef0433ce87a3ec
khsnh <ngx.k2210001@gmail.com>
2024-10-14 10:56:06 +0700

    add download button in topic import pop-up

---
+++ src/api/downloadFileApi.js
@@ -0,0 +1,15 @@
+import { get } from '@/axios'
+
+const url = '/files/download-template'
+
+class DownloadFileAPI {
+    get = (params) => {
+        return get(url, {
+            params,
+            responseType: 'blob'
+        })
+    }
+}
+
+const downloadFileAPI = new DownloadFileAPI()
+export default downloadFileAPI
